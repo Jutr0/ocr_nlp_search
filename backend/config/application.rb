5d068007ec64a549b99e3ecac988177a49ca80d9
wiktor <micwikski@gmail.com>
2025-04-23 20:34:09 +0200

    cleanup

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -40,5 +40,20 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.active_job.queue_adapter = :sidekiq
+
+    config.backend_url = ENV.fetch("BACKEND_URL", 'http://localhost:4000')
+    config.frontend_url = ENV.fetch('FRONTEND_URL', 'http://localhost:3000')
+
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+      g.test_framework :rspec,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: true,
+                       request_specs: true
+    end
+    Rails.application.routes.default_url_options[:host] = config.backend_url
   end
 end
