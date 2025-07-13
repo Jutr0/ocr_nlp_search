12f8f6c70ad13f21914ef034bd4d7672e608d576
wiktor <micwikski@gmail.com>
2025-07-13 19:24:24 +0200

    fix nil file error in CreateDocument + add specs for CreateDocument service

--- backend/app/modules/documents/services/documents/create_document.rb
+++ backend/app/modules/documents/services/documents/create_document.rb
@@ -3,7 +3,7 @@ module Documents
     include Interactor
 
     def call
-      document = Document.create!(status: :pending, file: context.file, filename: context.file.original_filename, user_id: context.user_id)
+      document = Document.create!(status: :pending, file: context.file, filename: context.file&.original_filename, user_id: context.user_id)
       context.document = document
       DocumentCreatedEvent.call(document)
     end
