ce253857a0369f99640d0009f052b27d9ff795d2
wiktor <micwikski@gmail.com>
2025-07-17 21:55:48 +0200

    tinkering with search

--- backend/app/modules/search/models/search.rb
+++ backend/app/modules/search/models/search.rb
@@ -2,6 +2,17 @@ require 'digest'
 
 module Search
   class Search
+
+    class Paths
+      BASE_DIR = "search"
+      WORDS_DIR = "#{BASE_DIR}/words"
+      DOCUMENTS_DIR = "#{BASE_DIR}/documents"
+      GRAVEYARD_DIR = "#{BASE_DIR}/graveyard"
+      WORDS = "#{WORDS_DIR}/words.json"
+      DOCUMENTS = "#{DOCUMENTS_DIR}/documents.json"
+      GRAVEYARD = "#{GRAVEYARD_DIR}/.graveyard"
+    end
+
     def initialize
       @documents = {}
       @words = {}
@@ -23,16 +34,31 @@ module Search
     end
 
     def search(query)
+      words = JSON.parse(File.read(Paths::WORDS))
+      documents = JSON.parse(File.read(Paths::DOCUMENTS))
       mapped_query = query.downcase.gsub(/[^a-z0-9\s]/i, '')
 
       found_documents = []
       mapped_query.split(" ").each do |word|
-        found_documents.concat(@words[word]) if @words[word].present?
+        found_documents.concat(words[word]) if words[word].present?
       end
 
       found_documents.uniq.map do |document_hash|
-        @documents[document_hash]
+        documents[document_hash]
       end
+    end
+
+    def flush!
+      words_json = @words.to_json
+      @words = {}
+      documents_json = @documents.to_json
+      @documents = {}
+
+      FileUtils.mkdir_p(Paths::WORDS_DIR)
+      File.write(Paths::WORDS, words_json)
+      FileUtils.mkdir_p(Paths::DOCUMENTS_DIR)
+      File.write(Paths::DOCUMENTS, documents_json)
+
     end
 
   end
--- backend/spec/modules/search/models/search_spec.rb
+++ backend/spec/modules/search/models/search_spec.rb
@@ -6,6 +6,7 @@ module Search
     describe 'searching' do
       it 'should return all documents with this words' do
         search = Search.new
+        search.flush!
 
         search.index("hello world")
         search.index("hello mate")
@@ -13,7 +14,12 @@ module Search
         search.index("Brown cat is black")
         search.index("Hello brown cat")
 
-        puts search.search("hell")
+        expect(search.search("hello")).to be_empty
+
+        search.flush!
+
+        expect(search.search("hello")).to eq(["hello world", "hello mate", "Hello brown cat"])
+        expect(search.search("Cat")).to eq(["Cat is black", "Brown cat is black", "Hello brown cat"])
 
       end
     end
